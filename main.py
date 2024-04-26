8b60a2fca9493c4a1071b7e250530fb2e8823dbc
Niktokton <151038902+Niktokton@users.noreply.github.com>
2024-04-26 19:56:26 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -26,108 +26,174 @@ def main():
     global username
     if request.method == 'POST':
         username = request.form['username']
-        return render_template('main.html', title='Квест "Скрытые секреты"', username=username, hide='unhidden')
+        return render_template('main.html',
+                               title='Квест "Скрытые секреты"',
+                               username=username,
+                               hide='unhidden')
     else:
-        return render_template('main.html', title='Квест "Скрытые секреты"', username=username, hide='hidden')
+        return render_template('main.html',
+                               title='Квест "Скрытые секреты"',
+                               username=username,
+                               hide='hidden')
 
 
 @app.route('/quest1', methods=['GET', 'POST'])
 def quest1():
-    if request.method == 'POST' and request.form['qst1'] == '1':
-        return render_template('quest_1.html', title='Квест "Скрытые секреты"', riddle='Загадка 1', fm='hidden',
+    if request.method == 'POST' and request.form['qst1'] == 'очень простой':
+        return render_template('quest_1.html',
+                               title='Квест "Скрытые секреты"',
+                               riddle='Загадка 1: Пароль',
+                               fm='hidden',
                                hide='unhidden')
     else:
-        return render_template('quest_1.html', title='Квест "Скрытые секреты"', riddle='Загадка 1', fm='unhidden',
+        return render_template('quest_1.html',
+                               title='Квест "Скрытые секреты"',
+                               riddle='Загадка 1: Пароль',
+                               fm='unhidden',
                                hide='hidden')
 
 
 @app.route('/quest2', methods=['GET', 'POST'])
 def quest2():
     if request.method == 'POST' and request.form['qst1'] == '1':
-        return render_template('quest_2.html', title='Квест "Скрытые секреты"', riddle='Загадка 2', fm='hidden',
+        return render_template('quest_2.html',
+                               title='Квест "Скрытые секреты"',
+                               riddle='Загадка 2: Будильник',
+                               fm='hidden',
                                hide='unhidden')
     else:
-        return render_template('quest_2.html', title='Квест "Скрытые секреты"', riddle='Загадка 2', fm='unhidden',
+        return render_template('quest_2.html',
+                               title='Квест "Скрытые секреты"',
+                               riddle='Загадка 2: Будильник',
+                               fm='unhidden',
                                hide='hidden')
 
 
 @app.route('/quest3', methods=['GET', 'POST'])
 def quest3():
-    if request.method == 'POST' and request.form['qst1'] == '1':
-        return render_template('quest_3.html', title='Квест "Скрытые секреты"', riddle='Загадка 3', fm='hidden',
+    if request.method == 'POST' and request.form['qst1'] == '2':
+        return render_template('quest_3.html',
+                               title='Квест "Скрытые секреты"',
+                               riddle='Загадка 3: Математики в баре',
+                               fm='hidden',
                                hide='unhidden')
     else:
-        return render_template('quest_3.html', title='Квест "Скрытые секреты"', riddle='Загадка 3', fm='unhidden',
+        return render_template('quest_3.html',
+                               title='Квест "Скрытые секреты"',
+                               riddle='Загадка 3: Математики в баре',
+                               fm='unhidden',
                                hide='hidden')
 
 
 @app.route('/quest4', methods=['GET', 'POST'])
 def quest4():
-    if request.method == 'POST' and request.form['qst1'] == '1':
-        return render_template('quest_4.html', title='Квест "Скрытые секреты"', riddle='Загадка 4', fm='hidden',
+    if request.method == 'POST' and request.form['qst1'] == '10':
+        return render_template('quest_4.html',
+                               title='Квест "Скрытые секреты"',
+                               riddle='Загадка 4: Рыбок жалко',
+                               fm='hidden',
                                hide='unhidden')
     else:
-        return render_template('quest_4.html', title='Квест "Скрытые секреты"', riddle='Загадка 4', fm='unhidden',
+        return render_template('quest_4.html',
+                               title='Квест "Скрытые секреты"',
+                               riddle='Загадка 4: Рыбок жалко',
+                               fm='unhidden',
                                hide='hidden')
 
 
 @app.route('/quest5', methods=['GET', 'POST'])
 def quest5():
     if request.method == 'POST' and request.form['qst1'] == '1':
-        return render_template('quest_5.html', title='Квест "Скрытые секреты"', riddle='Загадка 5', fm='hidden',
+        return render_template('quest_5.html',
+                               title='Квест "Скрытые секреты"',
+                               riddle='Загадка 5: Кошки в коробке',
+                               fm='hidden',
                                hide='unhidden')
     else:
-        return render_template('quest_5.html', title='Квест "Скрытые секреты"', riddle='Загадка 5', fm='unhidden',
+        return render_template('quest_5.html',
+                               title='Квест "Скрытые секреты"',
+                               riddle='Загадка 5: Кошки в коробке',
+                               fm='unhidden',
                                hide='hidden')
 
 
 @app.route('/quest6', methods=['GET', 'POST'])
 def quest6():
-    if request.method == 'POST' and request.form['qst1'] == '1':
-        return render_template('quest_6.html', title='Квест "Скрытые секреты"', riddle='Загадка 6', fm='hidden',
+    if request.method == 'POST' and request.form['qst1'] == '18':
+        return render_template('quest_6.html',
+                               title='Квест "Скрытые секреты"',
+                               riddle='Загадка 6: Треугольники',
+                               fm='hidden',
                                hide='unhidden')
     else:
-        return render_template('quest_6.html', title='Квест "Скрытые секреты"', riddle='Загадка 6', fm='unhidden',
+        return render_template('quest_6.html',
+                               title='Квест "Скрытые секреты"',
+                               riddle='Загадка 6: Треугольники',
+                               fm='unhidden',
                                hide='hidden')
 
 
 @app.route('/quest7', methods=['GET', 'POST'])
 def quest7():
     if request.method == 'POST' and request.form['qst1'] == '1':
-        return render_template('quest_7.html', title='Квест "Скрытые секреты"', riddle='Загадка 7', fm='hidden',
+        return render_template('quest_7.html',
+                               title='Квест "Скрытые секреты"',
+                               riddle='Загадка 7: Асфальт',
+                               fm='hidden',
                                hide='unhidden')
     else:
-        return render_template('quest_7.html', title='Квест "Скрытые секреты"', riddle='Загадка 7', fm='unhidden',
+        return render_template('quest_7.html',
+                               title='Квест "Скрытые секреты"',
+                               riddle='Загадка 7: Асфальт',
+                               fm='unhidden',
                                hide='hidden')
 
 
 @app.route('/quest8', methods=['GET', 'POST'])
 def quest8():
-    if request.method == 'POST' and request.form['qst1'] == '1':
-        return render_template('quest_8.html', title='Квест "Скрытые секреты"', riddle='Загадка 8', fm='hidden',
+    if request.method == 'POST' and request.form['qst1'] == '87':
+        return render_template('quest_8.html',
+                               title='Квест "Скрытые секреты"',
+                               riddle='Загадка 8: Машина на парковке',
+                               fm='hidden',
                                hide='unhidden')
     else:
-        return render_template('quest_8.html', title='Квест "Скрытые секреты"', riddle='Загадка 8', fm='unhidden',
+        return render_template('quest_8.html',
+                               title='Квест "Скрытые секреты"',
+                               riddle='Загадка 8: Машина на парковке',
+                               fm='unhidden',
                                hide='hidden')
 
 
 @app.route('/quest9', methods=['GET', 'POST'])
 def quest9():
-    if request.method == 'POST' and request.form['qst1'] == '1':
-        return render_template('quest_9.html', title='Квест "Скрытые секреты"', riddle='Загадка 9', fm='hidden',
+    if request.method == 'POST' and request.form['qst1'] == 'случайно':
+        return render_template('quest_9.html',
+                               title='Квест "Скрытые секреты"',
+                               riddle='Загадка 9: Производственный брак',
+                               fm='hidden',
                                hide='unhidden')
     else:
-        return render_template('quest_9.html', title='Квест "Скрытые секреты"', riddle='Загадка 9', fm='unhidden',
+        return render_template('quest_9.html',
+                               title='Квест "Скрытые секреты"',
+                               riddle='Загадка 9 : Производственный брак',
+                               fm='unhidden',
                                hide='hidden')
 
 
 @app.route('/quest10', methods=['GET', 'POST'])
 def quest10():
-    if request.method == 'POST' and request.form['qst1'] == '1':
-        return render_template('quest_10.html', title='Квест "Скрытые секреты"', riddle='Загадка 10', fm='hidden',
+    if request.method == 'POST':
+        return render_template('quest_10.html',
+                               title='Квест "Скрытые секреты"',
+                               riddle='Загадка 10: Жак Фреско',
+                               fm='hidden',
                                hide='unhidden')
     else:
-        return render_template('quest_10.html', title='Квест "Скрытые секреты"', riddle='Загадка 10', fm='unhidden',
+        return render_template('quest_10.html',
+                               title='Квест "Скрытые секреты"',
+                               riddle='Загадка 10: Жак Фреско',
+                               fm='unhidden',
                                hide='hidden')
 
 
@@ -142,7 +208,11 @@ def end():
     db_sess.commit()
     db_sess = db_session.create_session()
     resu = db_sess.query(result)
-    return render_template('end.html', title='Квест "Скрытые секреты"', username=username, timer=time, result=resu)
+    return render_template('end.html',
+                           title='Квест "Скрытые секреты"',
+                           username=username,
+                           timer=time,
+                           result=resu)
 
 
 @app.route("/_timer", methods=["GET", "POST"])
---
+++ static/script/quest.js
@@ -0,0 +1,76 @@
+function check1()
+{
+if (user.qst1.value != 'очень простой')
+    alert ('Неправильно');
+else
+    alert ('Правильно');
+}
+
+function check2()
+{
+if (user.qst1.value != '1')
+    alert ('Неправильно');
+else
+    alert ('Правильно');
+}
+
+function check3()
+{
+if (user.qst1.value != '2')
+    alert ('Неправильно');
+else
+    alert ('Правильно');
+}
+
+function check4()
+{
+if (user.qst1.value != '10')
+    alert ('Неправильно');
+else
+    alert ('Правильно');
+}
+
+function check5()
+{
+if (user.qst1.value != '1')
+    alert ('Неправильно');
+else
+    alert ('Правильно');
+}
+
+function check6()
+{
+if (user.qst1.value != '18')
+    alert ('Неправильно');
+else
+    alert ('Правильно');
+}
+
+function check7()
+{
+if (user.qst1.value != '1')
+    alert ('Неправильно');
+else
+    alert ('Правильно');
+}
+
+function check8()
+{
+if (user.qst1.value != '87')
+    alert ('Неправильно');
+else
+    alert ('Правильно');
+}
+
+function check9()
+{
+if (user.qst1.value != 'случайно')
+    alert ('Неправильно');
+else
+    alert ('Правильно');
+}
+
+function check10()
+{
+    alert ('Да');
+}
